c931ca2fe7c01b7ce4422e7e5637942262cb50ae
IceStorm935 <122942842+IceStorm935@users.noreply.github.com>
2023-01-18 16:06:28 -0700

    Update post_install_debian.sh

--- Install/post_install_debian.sh
+++ Install/post_install_debian.sh
@@ -5,43 +5,53 @@ sudo apt autoremove
 # Install Software
 dpkg --add-architecture i386
 sudo apt update && sudo apt upgrade -y
-SOFTWARE= cat ~/packages.txt
+SOFTWARE= cat ~/Downloads/packages.txt
 sudo apt install -y  $SOFTWARE
 
 # qtile
 pip install qtile
+pip install psutil
+sudo touch /usr/share/xsessions/qtile.desktop
+echo "
+[Desktop Entry]
+Name=Qtile
+Comment=Qtile Session
+Exec=qtile start
+Type=Application
+Keywords=wm;tiling
+" > /usr/share/xsessions/qtile.desktop
 
 # multiload-ng
-git clone https://github.com/udda/multiload-ng ~/patriot/
-cd ~/multiload-ng
+git clone https://github.com/udda/multiload-ng /home/patriot/
+cd /home/patriot/multiload-ng
 ./autogen.sh
 ./configure --prefix=/usr
+make
+sudo make install
 cd
 
 # .configs
-mkdir ~/Documents/My\ Configs/
-git clone https://github.com/IceStorm935/Muh_Private_Configs.git -C ~/Documents/My\ Configs
-mv ~/My\ Configs/* ~/.config/
+mkdir /home/patriot/Documents/My\ Configs/
+git clone https://github.com/IceStorm935/Muh_Private_Configs.git -C /home/patriot/Documents/My\ Configs
+mv /home/patriot/Documents/My\ Configs/* /home/patriot/.config/
 
 # .bashrc
-echo "
 alias upd='sudo apt update && sudo apt upgrade'
 alias autorm='sudo apt autoremove'
 alias add='sudo apt install'
 alias del='sudo apt remove'
-alias neo='neofetch --ascii_distro debian
+alias neo='neofetch --ascii_distro debian'
 alias hollywood='cd ~/Downloads/ && ./eDEX-UI-Linux-x86_64.AppImage'
 alias vpn='cat ~/Downloads/VPN.txt && sudo openvpn ~/Downloads/mx.protonvpn.net.udp.ovpn'
-" > ~/.bashrc
 source .bashrc
 
 # Automount SSD
 sudo blkid | grep sda > UUID.txt
-echo "UUID.txt" > /etc/fstab
 sudo mkdir /mnt/sda
-sudo mount -a
 
 # Virt-Manager
 sudo rc-service libvirtd start
 sudo rc-update add libvirtd
 sudo usermod -aG libvirt patriot
+
+echo "Place the UUID in fstab, then mount with sudo mount -a"
