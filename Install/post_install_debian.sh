a0b1d4622cd3611cf4f0075596203319f8e3d072
IceStorm935 <122942842+IceStorm935@users.noreply.github.com>
2023-01-17 17:11:19 -0700

    Add files via upload

---
+++ Install/post_install_debian.sh
@@ -0,0 +1,35 @@
+# Install Software
+SOFTWARE= cat packages.txt
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y  $SOFTWARE
+
+# qtile
+pip install qtile
+
+# multiload-ng
+git clone https://github.com/udda/multiload-ng
+./autogen.sh
+./configure --prefix=/usr
+
+# .configs
+mkdir ~/Documents/My\ Configs/
+git clone https://github.com/IceStorm935/Muh_Private_Configs.git -C ~/Documents/My\ Configs
+mv ~/My\ Configs/* ~/.config/
+
+# .bashrc
+echo "
+alias upd='sudo apt update && sudo apt upgrade'
+alias autorm='sudo apt autoremove'
+alias add='sudo apt install'
+alias del='sudo apt remove'
+alias neo='neofetch --ascii_distro debian
+alias hollywood='cd ~/Downloads/ && ./eDEX-UI-Linux-x86_64.AppImage'
+alias vpn='cat ~/Downloads/VPN.txt && sudo openvpn ~/Downloads/mx.protonvpn.net.udp.ovpn'
+" > ~/.bashrc
+source .bashrc
+
+# Automount SSD
+sudo blkid | grep sda > UUID.txt
+echo "UUID.txt" > /etc/fstab
+sudo mkdir /mnt/sda
+sudo mount -a
